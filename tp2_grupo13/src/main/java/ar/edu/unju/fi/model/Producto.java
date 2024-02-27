543595a1c4922386b4bb8dabe0c20519e64b394f
LuVeliz <guadalupeveliz538@gmail.com>
2024-02-27 18:41:11 -0300

    Agregue varias cosas al proyecto e.e thymeleaf, controladores

---
+++ tp2_grupo13/src/main/java/ar/edu/unju/fi/model/Producto.java
@@ -0,0 +1,53 @@
+ package ar.edu.unju.fi.model;
+
+public class Producto {
+	String nombre;
+	Integer codigo;
+	Double precio;
+	String categoria;
+	int descuento;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public int getDescuento() {
+		return descuento;
+	}
+	public void setDescuento(int descuento) {
+		this.descuento = descuento;
+	}
+	public Producto(String nombre, Integer codigo, Double precio, String categoria, int descuento) {
+		super();
+		this.nombre = nombre;
+		this.codigo = codigo;
+		this.precio = precio;
+		this.categoria = categoria;
+		this.descuento = descuento;
+	}
+	
+	public Double calcularDescuento( Double precio,int descuento) {
+		double resultado;
+		resultado=precio-descuento;
+		return resultado;
+	}
+}
